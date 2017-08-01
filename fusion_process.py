592d6449f29004ec9c6f589a289729db8159a33f
dcooper46 <daniel.cooper@publicismedia.com>
2017-08-01 13:02:49 -0500

    bug fixes

--- fusion_process.py
+++ fusion_process.py
@@ -87,7 +87,10 @@ def entropy(x):
 def scale(arr):
     if arr.ndim < 2:
         return np.nan_to_num((arr - arr.mean()) / arr.std())
-    return np.nan_to_num((arr - arr.mean(axis=1, keepdims=True)) / arr.std(axis=1, keepdims=True))
+    return np.nan_to_num(
+            (arr - arr.mean(axis=1, keepdims=True))
+            / arr.std(axis=1, keepdims=True)
+        )
 
 
 def score(dist, p=2):
@@ -98,15 +101,19 @@ def wgtd_score(dist, wgts, p=2):
     return np.power(abs(sum(wgts * np.sign(dist) * np.power(abs(dist), p))), p)
 
 
+def grange(x):
+    return np.max(x) - np.min(x)
+
+
 def gower(x, y, wgts):
     def cat_equal(x, y):
         return np.not_equal(x, y)
 
-    def edu_dist(x, y):
-        return abs(x - y) / 7  # range of education values
+    def cat_dist(x, y, d):
+        return abs(x - y) / d  # range of education values
 
     d = cat_equal(x, y).astype(float)
-    d.put(2, edu_dist(x[2], y[2]))
+    d.put(2, cat_dist(x[2], y[2], grange(np.concatenate((x, y)))))
 
     return (d * wgts).sum() / wgts.sum()
 
@@ -123,6 +130,8 @@ def get_fusion_vars(indices, donors, fvars, method='mode'):
     matches = donors.take(indices)[fvars]
     if method == 'mode':
         return matches.mode().mean().astype(int)
+    elif method == 'random':
+        return matches.take(np.random.randint(matches.shape[0]), axis=0)
     else:
         raise NotImplementedError
 
@@ -130,21 +139,26 @@ def get_fusion_vars(indices, donors, fvars, method='mode'):
 def get_matches(distances, method, **kwargs):
     if method == 'knn':
         return distances.argsort(axis=1)[:, :k]
+    else:
+        raise NotImplementedError
 
 
 def fuse(donors, recipients, linking, fusion, critical=None, imp_wgts=None,
          matching='knn', donate_by='mode', njobs=2, k=10):
-
-    if not critical:
-        if imp_wgts:  # try to group by variable with highest weight
+    if critical is None:
+        if imp_wgts:
+            # try to group by variable with highest weight
             critical = linking[imp_wgts.argmax()]
-        else:         # take variable with most unique values to ensure smallest groups
-            critical = linking[np.argmax([recipients[var].unique().size for var in linking])]
+        else:
+            # take variable with most unique values to ensure smallest groups
+            critical = linking[np.argmax(
+                [recipients[var].unique().size for var in linking]
+            )]
 
-    cc_donors = donors.groupby(critical, sort=False) # turn off sorting is better performance
+    cc_donors = donors.groupby(critical, sort=False)
     cc_recips = recipients.groupby(critical, sort=False)
 
-    if not imp_wgts:
+    if imp_wgts is None:
         imp_wgts = np.ones(len(linking))
 
     # can use custom distance or standard like Gower (handles mixed types)
@@ -155,8 +169,10 @@ def fuse(donors, recipients, linking, fusion, critical=None, imp_wgts=None,
         donors_cc = cc_donors.get_group(name)
         dist = pairwise_distances(recips_cc[linking], donors_cc[linking],
                                   gower, n_jobs=njobs, wgts=imp_wgts)
-        top_matches = dist.argsort(axis=1)[:,:k]
-        fused_vars = np.apply_along_axis(get_fusion_vars, 1, top_matches, donors_cc, fusion)
+        top_matches = get_matches(dist, matching, k=k)
+        fused_vars = np.apply_along_axis(
+            get_fusion_vars, 1, top_matches, donors_cc, fusion, donate_by
+        )
         fused_vars = pd.DataFrame(fused_vars).set_index(recips_cc.index)
         fused_vars.columns = fusion
         fused_data[name] = recips_cc.join(fused_vars, how='right')
@@ -164,46 +180,56 @@ def fuse(donors, recipients, linking, fusion, critical=None, imp_wgts=None,
     print("finished matching in {} seconds".format(time() - t0))
 
     fused_df = fused_data[1]
-    fused_df = fused_df.append([df for key, df in fused_data.items() if key != 1])
+    fused_df = fused_df.append(
+        [df for key, df in fused_data.items() if key != 1]
+    )
 
     return fused_df
 
 
-def main(files, vars_, k=10):
+def main(files, vars_, matching='knn', k=10, donate_by='mode'):
     donors, recipients, maps = load_data(files)
 
-    linking_vars, fusion_vars, critical_vars = map(maps.get, ['linking', 'fusion', 'critical'])
+    linking_vars, fusion_vars, critical_vars = map(
+        vars_.get, ['linking_vars', 'fusion_vars', 'critical_vars']
+    )
 
     imp_wgts = importance_wgts(recipients.append(donors)[linking_vars])
 
     fused_data = fuse(donors, recipients, linking_vars, fusion_vars,
-                      critical_vars, imp_wgts, k)
+                      critical_vars, imp_wgts, matching=matching,
+                      donate_by=donate_by, k=k)
 
-    fused_data.to_csv("fused_data_{}nn-mode.csv".format(k), index=False)
+    fused_data.to_csv("fused_data_{}nn-rand.csv".format(k), index=False)
 
 
 if __name__ == "__main__":
 
-    files = {}
-    files['donor'] = 'donors.csv'
-    files['recipient'] = 'recipients.csv'
+    files = dict()
+    files['donors'] = 'donors.csv'
+    files['recipients'] = 'recipients.csv'
     files['maps'] = 'maps.npy'
 
-    vars_ = dict(critical_vars = ['age_enc'],
-                 linking_vars = ['job_enc', 'marital_enc', 'education_enc',
-                                 'default_enc', 'housing_enc', 'loan_enc'] ,
-                 target = ['y'],
-                 fusion_vars = ['contact_enc', 'month_enc', 'day_of_week_enc',
-                                'campaign_enc', 'pdays_enc', 'previous_enc', 'poutcome_enc'])
+    vars_ = dict(critical_vars=['age_enc'],
+                 linking_vars=['job_enc', 'marital_enc', 'education_enc',
+                               'default_enc', 'housing_enc', 'loan_enc'],
+                 target=['y'],
+                 fusion_vars=[
+                    'contact_enc', 'month_enc', 'day_of_week_enc',
+                    'campaign_enc', 'pdays_enc', 'previous_enc', 'poutcome_enc'
+                 ])
+    match_mthd = 'knn'
+    donate_mthd = 'random'
     k = 10
 
-    main(files, vars_, k)
+    main(files, vars_, matching=match_mthd, donate_by=donate_mthd, k=k)
 
-#donated = []
-#for idx, row in recipients.iterrows():
+# donated = []
+# for idx, row in recipients.iterrows():
 #    cc = cc_donors.get_group(row.age_enc)
 #    rr = row[linking_vars].values
-#    distances = np.apply_along_axis(gower, 1, cc[linking_vars].values, rr, imp_wgts)
+#    distances = np.apply_along_axis(
+#           gower, 1, cc[linking_vars].values, rr, imp_wgts)
 #    matches = distances.argsort()[:k]
 #
 #    # take mode of fused variables for matches
